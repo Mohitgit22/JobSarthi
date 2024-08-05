dd6932a0d257afd87c328f559661967c00712121
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2024-08-05 15:55:49 +0530

    Added corrected code

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://deploy-mern-frontend-inky.vercel.app/',
+    origin:'https://deploy-mern-frontend-inky.vercel.app',
     credentials:true
 }
 
