b3bf4a1eadac2e89e1148efea3afdb51586fb24d
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2024-12-13 23:22:23 +0530

    edited css codes

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -29,13 +29,13 @@ const Navbar = () => {
         }
     }
     return (
-        <div className='bg-white'>
+        <div className='bg-gradient-to-r from-gray-800 via-gray-900 to-black'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
+                    <h1 className='text-2xl font-bold text-white'>Job<span className='text-white'>Sarthi</span></h1>
                 </div>
                 <div className='flex items-center gap-12'>
-                    <ul className='flex font-medium items-center gap-5'>
+                    <ul className='flex font-medium items-center gap-5 text-white'>
                         {
                             user && user.role === 'recruiter' ? (
                                 <>
